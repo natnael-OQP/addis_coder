4df6f19b9b0d62caae0d7174e9c7708fa14123c5
natnael-OQP <makiya112545@gmail.com>
2022-08-26 23:28:34 +0300

    welcome_screen completed

--- lib/screen/welcome_screen.dart
+++ lib/screen/welcome_screen.dart
@@ -1,5 +1,6 @@
 import 'package:addis_coder/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -11,30 +12,115 @@ class WelcomeScreen extends StatelessWidget {
       body: Stack(
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
+            padding: const EdgeInsets.only(bottom: 20),
             child: Expanded(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
+                  SizedBox(
+                    child: Container(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const [
+                          Spacer(),
+                          Text("Skip >"),
+                        ],
+                      ),
+                    ),
+                  ),
                   // logo
                   Image.asset(
-                    "/assets/images/black logo.png",
+                    "assets/images/black logo.png",
                     width: double.infinity,
-                    height: 100,
+                    height: 170,
                   ),
                   // text
-                  Column(
-                    children: [
-                      Text(
-                        "Welcome",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: primary,
-                          fontSize: 24,
+                  SizedBox(
+                    width: 200,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Welcome",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: primary,
+                            fontSize: 30,
+                          ),
+                        ),
+                        const Gap(10),
+                        const Text(
+                          "Learning to code is learning to create and innovate",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12,
+                          ),
+                        ),
+                        const Gap(60),
+                        // indicator
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.all(5),
+                              width: 12,
+                              height: 12,
+                              decoration: BoxDecoration(
+                                color: primary,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.all(5),
+                              width: 12,
+                              height: 12,
+                              decoration: BoxDecoration(
+                                color: lightGray,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.all(5),
+                              width: 12,
+                              height: 12,
+                              decoration: BoxDecoration(
+                                color: lightGray,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  // button
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: Expanded(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: primary,
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            "Let's Code",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
                       ),
-                    ],
-                  )
+                    ),
+                  ),
                 ],
               ),
             ),
