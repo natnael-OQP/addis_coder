1b4ea44efc18a8296c7bd4aa007f3bd7fb9013ec
natnael-OQP <makiya112545@gmail.com>
2022-08-26 23:25:18 +0300

    bg color and primary color created

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 Color bgColor = HexColor("#FFF");
-Color primary = HexColor("#FF4B3A");
+Color primary = HexColor("#E94535");
 Color lightGray = HexColor("#E9E8E8");
