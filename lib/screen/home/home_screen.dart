22b15cece4641077518677cb1dbbedd31ec43c13
natnael-OQP <makiya112545@gmail.com>
2022-08-29 18:17:31 +0300

    style header screen

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -7,18 +7,51 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("task app"),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: Image.asset("assets/icons/menu.png"),
+        title: const Text(
+          "Python",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
         actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.add),
+          Stack(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.all(Radius.circular(30)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 1,
+                        offset:
+                            const Offset(0, 3), // changes position of shadow
+                      ),
+                    ]),
+                child: Image.asset("assets/icons/person.png"),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 12.0, left: 6),
+                child: Image.asset("assets/icons/notification.png"),
+              ),
+            ],
           ),
         ],
       ),
       body: Column(
         children: const [
           Center(
-            child: Text("tasks:"),
+            child: Text(
+              "Python",
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
           ),
         ],
       ),
