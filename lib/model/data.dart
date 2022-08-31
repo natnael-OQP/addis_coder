4104120cbd0f93bb7f015cb6089d5c51af6796fd
natnael-OQP <makiya112545@gmail.com>
2022-08-31 10:33:44 +0300

    create random data

---
+++ lib/model/data.dart
@@ -0,0 +1,46 @@
+class Events {
+  final String image, title;
+
+  Events({
+    required this.image,
+    required this.title,
+  });
+}
+
+List<Events> demo_events = [
+  Events(
+    image:
+        "https://imgs.search.brave.com/GrK3WWr-YqkwAhG6QcAaRG9Nl4Y6376qNBEWXf7i3QU/rs:fit:851:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5G/TTZUM3lnVTFvdW5h/T0QwWjlIMTVBSGFF/SSZwaWQ9QXBp",
+    title: "Php",
+  ),
+  Events(
+    image:
+        "https://imgs.search.brave.com/FWkNroWb6lf3njUHSQcpWkv0q4A2KnKplf5o1MRkG4o/rs:fit:1200:1152:1/g:ce/aHR0cHM6Ly93d3cu/ZmlsZXBpY2tlci5p/by9hcGkvZmlsZS9U/eTBZNnpaaFEwU0hr/T1YyTzFkUw",
+    title: "Python",
+  ),
+  Events(
+    image:
+        "https://imgs.search.brave.com/BekEvyZOnoYWl7L1LXkD8g5IgWi04Y9Pk6Tqm1UUvMs/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5u/TVZpNUtCX2IyU09N/cWhZNGM0Zlp3SGFF/OCZwaWQ9QXBp",
+    title: "React",
+  ),
+  Events(
+    image:
+        "https://imgs.search.brave.com/yWASR7SdLaZgR1YGCMpxUsJwHn0b4ehfCRI8EnN8ABM/rs:fit:361:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC50/VklxbkdSaGxHeTl1/ejd5UGtyUVl3SGFK/dCZwaWQ9QXBp",
+    title: "Week1",
+  ),
+  Events(
+    image:
+        "https://imgs.search.brave.com/BekEvyZOnoYWl7L1LXkD8g5IgWi04Y9Pk6Tqm1UUvMs/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5u/TVZpNUtCX2IyU09N/cWhZNGM0Zlp3SGFF/OCZwaWQ9QXBp",
+    title: "Week2",
+  ),
+  Events(
+    image:
+        "https://imgs.search.brave.com/BekEvyZOnoYWl7L1LXkD8g5IgWi04Y9Pk6Tqm1UUvMs/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5u/TVZpNUtCX2IyU09N/cWhZNGM0Zlp3SGFF/OCZwaWQ9QXBp",
+    title: "Week2",
+  ),
+  Events(
+    image:
+        "https://imgs.search.brave.com/BekEvyZOnoYWl7L1LXkD8g5IgWi04Y9Pk6Tqm1UUvMs/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5u/TVZpNUtCX2IyU09N/cWhZNGM0Zlp3SGFF/OCZwaWQ9QXBp",
+    title: "Week2",
+  ),
+];
