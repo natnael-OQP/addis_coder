ef71646c3915519b613d3d758b7ed6b67471cfb3
natnael-OQP <makiya112545@gmail.com>
2022-08-31 11:36:31 +0300

    types section completed

--- lib/screen/type/types_screen.dart
+++ lib/screen/type/types_screen.dart
@@ -8,6 +8,9 @@ class TypesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: HexColor("#FFF"),
       appBar: AppBar(
@@ -52,15 +55,16 @@ class TypesScreen extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(10),
             child: Column(
-              children: const [
-                Text(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
                   "Here are the basic types in Python that you should know about:",
                   style: TextStyle(
                     fontSize: 12,
                     height: 1.55,
                   ),
                 ),
-                BulletList(
+                const BulletList(
                   [
                     "int: an integer.",
                     "float: a decimal number.",
@@ -70,22 +74,133 @@ class TypesScreen extends StatelessWidget {
                     "dict: a mapping from keys to values (we will learn more about these later)"
                   ],
                 ),
-                Text(
+                const Text(
                   "Variables contain a value, so imagine a variable as a cup that contains something. However, we use different types of cups for different beverages; coffee cups are different from water cups, which are different from tea cups. Imagine types as giving a cup, or a variable, the unique type. By assigning an integer value to a variable, we give our cup a more specific purpose.",
                   style: TextStyle(
                     fontSize: 12,
                     height: 1.55,
                   ),
                 ),
-                Gap(15),
-                Text(
+                const Gap(15),
+                const Text(
                   "Let's look at examples of how different types interact with each other in Python.",
                   style: TextStyle(
                     fontSize: 12,
                     height: 1.55,
                   ),
                 ),
+                // output
+                OutputCode(
+                  height: height,
+                  width: width,
+                  index: 1,
+                  input: "print(type(15))",
+                  output: "<class 'int'>",
+                ),
+                OutputCode(
+                  height: height,
+                  width: width,
+                  index: 2,
+                  input: "print(type(15.5))",
+                  output: "<class 'float'>",
+                ),
               ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OutputCode extends StatelessWidget {
+  const OutputCode({
+    Key? key,
+    required this.height,
+    required this.width,
+    required this.index,
+    required this.input,
+    required this.output,
+  }) : super(key: key);
+
+  final double height;
+  final double width;
+  final int index;
+  final String input, output;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: height * .03),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "$index.",
+                style: const TextStyle(
+                  color: Colors.red,
+                  fontSize: 16,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(left: width * .03),
+                  height: 40,
+                  decoration: BoxDecoration(
+                    color: HexColor("#f2f2f2"),
+                    border: const Border(
+                      top: BorderSide(width: 1.0, color: Colors.black26),
+                      left: BorderSide(width: 1.0, color: Colors.black26),
+                      right: BorderSide(width: 1.0, color: Colors.black26),
+                      bottom: BorderSide(width: 1.0, color: Colors.black26),
+                    ),
+                  ),
+                  child: Text(
+                    input,
+                    style: const TextStyle(
+                      color: Colors.black26,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: width / 3.9,
+              vertical: height * .005,
+            ),
+            child: const Icon(
+              Icons.arrow_downward,
+              color: Colors.red,
+              size: 25,
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: width * .03),
+            margin: EdgeInsets.only(left: width * .03),
+            height: 40,
+            width: width / 1.5,
+            decoration: BoxDecoration(
+              color: HexColor("#000"),
+              border: const Border(
+                top: BorderSide(width: 1.0, color: Colors.black26),
+                left: BorderSide(width: 1.0, color: Colors.black26),
+                right: BorderSide(width: 1.0, color: Colors.black26),
+                bottom: BorderSide(width: 1.0, color: Colors.black26),
+              ),
+            ),
+            child: Text(
+              output,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+              ),
             ),
           ),
         ],
